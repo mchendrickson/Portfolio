073f7f1da1ca552191b1efbfa8c0f567647de2df
mchrpt <mchrpt@yahoo.com>
2023-03-07 19:48:17 -0500

    initial commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,41 +15,22 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
+            <h1 style={{ fontSize: "3.0em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I enjoy programming unnecessarily complex things both at work and in my free time.
               <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Python. </b>
-              </i>
+              <br />I am fluent in object oriented languages such as <i><b className="purple"> Java, C#, Javascript, and Python. </b></i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
-              </i>
+              I hope to get a job as a <i><b className="purple">systems programmer</b></i> or other coding role at a popular game studio. I specialize in RTS games, but I have a love for a variety of other genres too such as FPS, 4X, TBS, and VR games. &nbsp;
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I have plenty of experience in <i><b className="purple">managing</b></i> software development teams.
+              <br />
+              <br />
+              I'm pursuing a double major in <i><b className="purple"> Computer Science </b></i> and <i><b className="purple"> Interactive Media and Game Technology </b></i> with a 4.0 GPA at Worcester Polytechnic Institute.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Game Designer",
+          "Computer Scientist",
+          "Systems Developer",
+          "Software Engineer",
         ],
         autoStart: true,
         loop: true,
