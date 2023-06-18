c2a6cb0515732b4c0ea0e7f1fed64b06dbb7b732
mchrpt <mchrpt@yahoo.com>
2023-06-18 17:16:40 -0400

    Better formatting of p5js

--- src/components/Projects/ProjectPages/Pfivejs.js
+++ src/components/Projects/ProjectPages/Pfivejs.js
@@ -9,15 +9,15 @@ function Pfivejs() {
                 <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
                     Various p5.js Projects
                 </h1>
-                <Row style={{ justifyContent: "center", padding: "12px" }}>
-                    <Col md={6}>
+                <Row>
+                    <Col >
                         <iframe
                             src="https://editor.p5js.org/mchendrickson/full/FIyhwA0D0"
                             title="p5js iframe"
                             style={{ border: "none", width: "600px", height: "600px" }}
                         ></iframe>
                     </Col>
-                    <Col md={6}>
+                    <Col >
                         <iframe
                             src="https://editor.p5js.org/mchendrickson/full/8nI90LLo9"
                             title="p5js iframe"
@@ -25,15 +25,15 @@ function Pfivejs() {
                         ></iframe>
                     </Col>
                 </Row>
-                <Row style={{ justifyContent: "center", padding: "12px" }}>
-                    <Col md={6}>
+                <Row>
+                    <Col >
                         <iframe
                             src="https://editor.p5js.org/mchendrickson/full/Rq68bfS2Y"
                             title="p5js iframe"
                             style={{ border: "none", width: "600px", height: "600px" }}
                         ></iframe>
                     </Col>
-                    <Col md={6}>
+                    <Col>
                         <iframe
                             src="https://editor.p5js.org/mchendrickson/full/74XLtdnMY"
                             title="p5js iframe"
