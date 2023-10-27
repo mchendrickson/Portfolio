58ac74e78b3406b3c7b518ab65d5c52aaffeeee0
mchrpt <mchrpt@yahoo.com>
2023-10-27 14:57:30 -0400

    Quick bugfix for the Resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/mchrpt/portfolio/master/src/Assets/mchendrickson-resume.pdf";
+  "https://raw.githubusercontent.com/mchrpt/portfolio/master/src/Assets/Matthew Hendrickson Resume Web.pdf";
 
 function Resume() {
   const [width, setWidth] = useState(1200);
