deaa9a9786339e6337b7e40d3518bbce5b39b9c5
mchrpt <mchrpt@yahoo.com>
2025-03-26 15:21:17 -0400

    Cleanup code on about page

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import Particle from "../Particle";
-//import Github from "./Github";
 import TechStackAll from "./TechStackAll";
-import Aboutcard from "./AboutCard";
+import AboutCard from "./AboutCard";
 import ToolStack from "./ToolStack";
 import Tilt from "react-parallax-tilt";
 import myImg from "../../Assets/matt-photo-edited2.png";
@@ -11,45 +10,37 @@ import myImg from "../../Assets/matt-photo-edited2.png";
 function About() {
     return (
         <Container fluid className="about-section">
-            <Particle/>
             <Container>
+                {/* About Me Section */}
                 <Row style={{justifyContent: "center", padding: "10px"}}>
-                    <Col
-                        md={7}
-                        style={{
-                            justifyContent: "center",
-                            paddingTop: "30px",
-                            paddingBottom: "50px",
-                        }}
-                    >
+                    <Col md={7} style={{paddingTop: "30px", paddingBottom: "50px"}}>
                         <h1 style={{fontSize: "2.1em", paddingBottom: "20px"}}>
                             About <strong className="purple">Me</strong>
                         </h1>
-                        <Aboutcard/>
+                        <AboutCard/>
                     </Col>
-                    <Col
-                        md={5}
-                        style={{paddingTop: "50px", paddingBottom: "50px"}}
-                        className="about-img"
-                    >
+
+                    <Col md={5} style={{paddingTop: "50px", paddingBottom: "50px"}} className="about-img">
                         <Tilt>
                             <img src={myImg} className="img-fluid" alt="avatar"/>
                         </Tilt>
                     </Col>
                 </Row>
-                <h1 className="project-heading">
-                    Professional <strong className="purple">Skillset </strong>
-                </h1>
 
+                {/* Skillset */}
+                <h1 className="project-heading">
+                    Professional <strong className="purple">Skillset</strong>
+                </h1>
                 <TechStackAll/>
 
+                {/* Tools */}
                 <h1 className="project-heading">
                     <strong className="purple">Tools</strong> I use
                 </h1>
                 <ToolStack/>
-
-                {/*<Github />*/}
             </Container>
+
+            <Particle/>
         </Container>
     );
 }
