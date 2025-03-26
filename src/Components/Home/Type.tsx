d0734edd76b7c32b4028283b66f72996edc86cec
mchrpt <mchrpt@yahoo.com>
2025-03-26 16:46:05 -0400

    Made the typewriter effect use the preloader

--- src/Components/Home/Type.tsx
+++ src/Components/Home/Type.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { usePreloader } from "../../PreloaderContext";
 
 function Type() {
+    const { preloaderDone } = usePreloader();
+
     return (
-        <span style={{whiteSpace: "nowrap", display: "inline-block"}}>
-            <Typewriter
-                onInit={(typewriter) => {
-                    typewriter
-                        .typeString("Game Programmer")
-                        .start()
-                }}
-            />
+        <span style={{ whiteSpace: "nowrap", display: "inline-block" }}>
+            {preloaderDone && (
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Game Programmer")
+                            .start();
+                    }}
+                />
+            )}
         </span>
     );
 }
