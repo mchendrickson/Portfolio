8794f0d5a776b8b82bb1034700c65f2a5a27401b
mchrpt <mchrpt@yahoo.com>
2025-04-01 21:28:47 -0400

    Rewrite hardpoints section to be more clear

--- src/Components/Projects/ProjectPages/Imperius/HardpointsSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/HardpointsSection.tsx
@@ -6,10 +6,11 @@ import VimeoVideo from "../../../../VimeoVideo";
 const HardpointsSection: React.FC = () => {
     return (
         <Section title="Hardpoints">
-            <p style={{ textAlign: "left" }}>
-                Hardpoints are <b className="purple">destructible subsystems</b>—such as weapons, engines, or other critical components—on units and buildings. These subsystems can be <b className="purple">individually targeted</b> and destroyed during combat, with some units featuring dozens of them. This mechanic introduces a new <b className="purple">strategic gameplay layer</b> for players to manage and exploit.
+            <p style={{textAlign: "left"}}>
+                Hardpoints are <b className="purple">destructible subsystems</b>—like weapons or engines—that can be <b
+                className="purple">individually targeted</b> to cripple a ship's functionality, adding another layer of
+                strategy for players to micromanage and exploit.
             </p>
-
 
             <Col
                 md={8}
