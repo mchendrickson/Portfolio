743ec4fd321ba6ef67bf23831ad6ee34d9560d2a
mchrpt <mchrpt@yahoo.com>
2025-04-02 00:06:59 -0400

    Fix scaling issue with images in Corporate Clash section on mobile

--- src/Components/Projects/ProjectPages/CorporateClash/ContributionsSection.tsx
+++ src/Components/Projects/ProjectPages/CorporateClash/ContributionsSection.tsx
@@ -5,17 +5,25 @@ import AstronPandas from "../../../../Assets/Projects/CorporateClash/AstronPanda
 
 const ContributionsSection: React.FC = () => {
     const bullets = [
-        <><b className="purple">Engine & Networking Integration:</b> Worked extensively with the Panda3D engine and Astron Networking Project to develop core systems and maintain online infrastructure.</>,
-        <><b className="purple">Security Enhancements:</b> Implemented client and server-side safety features to protect player experience and mitigate potential exploits.</>,
-        <><b className="purple">Content Development:</b> Designed and implemented new gameplay content while assisting in debugging and improving existing features.</>,
-        <><b className="purple">Legacy Refactoring:</b> Modernized legacy code to reduce technical debt, improve maintainability, and support long-term scalability.</>,
+        <><b className="purple">Engine & Networking Integration:</b> Worked extensively with the Panda3D engine and
+            Astron Networking Project to develop core systems and maintain online infrastructure.</>,
+        <><b className="purple">Security Enhancements:</b> Implemented client and server-side safety features to protect
+            player experience and mitigate potential exploits.</>,
+        <><b className="purple">Content Development:</b> Designed and implemented new gameplay content while assisting
+            in debugging and improving existing features.</>,
+        <><b className="purple">Legacy Refactoring:</b> Modernized legacy code to reduce technical debt, improve
+            maintainability, and support long-term scalability.</>,
     ];
 
     return (
         <Section title="My Contributions">
-            <BulletList items={bullets} />
-            <br />
-            <img src={AstronPandas} alt="about" className="img-fluid rounded" style={{ width: "60%" }} />
+            <BulletList items={bullets}/>
+            <br/>
+            <img
+                src={AstronPandas}
+                alt="Astron Pandas"
+                className="display-image-container rounded"
+            />
         </Section>
     );
 };
