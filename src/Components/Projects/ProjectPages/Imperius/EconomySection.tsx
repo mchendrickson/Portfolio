416d6e4445947af23ff51cb2b6b6de75ef4841ca
mchrpt <mchrpt@yahoo.com>
2025-04-02 14:47:41 -0400

    Replace economy static image with economy video

--- src/Components/Projects/ProjectPages/Imperius/EconomySection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/EconomySection.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Section from "../../Section";
 import BulletList from "../../BulletList";
 import ImperiusHomeMenu from "../../../../Assets/Projects/Imperius/ImperiusHomeMenu.png";
+import VimeoVideo from "../../../../VimeoVideo";
+import {Col} from "react-bootstrap";
 
 const EconomySection: React.FC = () => {
     const bullets = [
         <><b className="purple">Materials:</b> Used for constructing units, buildings, and unlocking upgrades. </>,
-        <><b className="purple">Power:</b> Caps at 100%; exceeding this limit restricts building/unit production and reduces material generation. </>,
         <><b className="purple">Tech:</b> Progresses through five tiers, each requiring more materials. Higher tiers unlock advanced buildings and high-cost units. </>,
+        <><b className="purple">Power:</b> Caps at 100%; exceeding this limit restricts building/unit production and reduces material generation. </>,
     ];
 
     return (
@@ -20,11 +22,13 @@ const EconomySection: React.FC = () => {
                 Players must balance each of these resources with base building, territory control, and unit production.
                 Mismanagement of any of these three will lead to dire strategic consequences.
             </p>
-            <img
-                src={ImperiusHomeMenu}
-                alt="Imperius Home Menu"
-                className="display-image-container rounded"
-            />
+            <Col
+                md={8}
+                className="vimeo-container"
+                style={{paddingTop: "10px"}}
+            >
+                <VimeoVideo videoId="1071905755" videoTitle="Imperius Economy"/>
+            </Col>
         </Section>
     );
 };
