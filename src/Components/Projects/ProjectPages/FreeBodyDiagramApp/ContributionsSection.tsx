e0c049804bde075f81d7af73c869e7897c47f0b4
mchrpt <mchrpt@yahoo.com>
2025-04-02 23:47:52 -0400

    Cleanup FBD contributions section

--- src/Components/Projects/ProjectPages/FreeBodyDiagramApp/ContributionsSection.tsx
+++ src/Components/Projects/ProjectPages/FreeBodyDiagramApp/ContributionsSection.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import Section from "../../Section";
 import BulletList from "../../BulletList";
 
 const ContributionsSection: React.FC = () => {
     const bullets = [
-        <>Refactored the <b className="purple">entire codebase</b> to align with <b className="purple">OOP methodologies</b>.</>,
-        <>Implemented <b className="purple">missing documentation</b> and <b className="purple">updated current documentation</b> to be more descriptive.</>,
-        <>Reworked <b className="purple">tracking documents</b> to be more concise and understandable.</>,
+        <>
+            <b className="purple">Codebase Refactor:</b> Rewrote major systems using OOP to improve structure, readability, and future scalability.
+        </>,
+        <>
+            <b className="purple">Documentation:</b> Created new internal docs and cleaned up legacy ones to support easier onboarding and maintenance.
+        </>,
+        <>
+            <b className="purple">Workflow Improvements:</b> Simplified internal tracking docs to reduce redundancy and streamline team processes.
+        </>,
     ];
+
+
 
     return (
         <Section title="My Contributions">
-            <BulletList items={bullets} />
-            <Col md={8} className="img-container" style={{ paddingTop: "10px" }}>
+            <BulletList items={bullets}/>
+            <Col md={8} className="img-container" style={{paddingTop: "10px"}}>
                 <Row>
                     <Col>
                         <img
