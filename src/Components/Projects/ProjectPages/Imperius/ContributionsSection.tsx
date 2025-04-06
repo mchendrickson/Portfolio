6b536ee31ce28998efb245ca8c528fad44c9ab73
mchrpt <mchrpt@yahoo.com>
2025-04-06 13:25:44 -0400

    Another fix to wording on contributions bullet point

--- src/Components/Projects/ProjectPages/Imperius/ContributionsSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/ContributionsSection.tsx
@@ -7,7 +7,7 @@ const ContributionsSection: React.FC = () => {
     const bullets = [
         <><span className="purple">Project Manager:</span> Recruited, coordinated, and managed a team of 6.</>,
         <><span className="purple">Lead Programmer:</span> Authored most of the 15,000+ line codebase and managed version control for team collaboration.</>,
-        <><span className="purple">Systems Programmer:</span> Designed and implemented several crucial game mechanics listed below.</>,
+        <><span className="purple">Systems Programmer:</span> Designed and implemented the majority of the game's mechanics.</>,
         <><span className="purple">Audio Programmer:</span> Integrated FMOD middleware to ensure authentic and realistic soundscapes.</>,
         <><span className="purple">VFX Tech Artist:</span> Developed and adapted a comprehensive range of VFX assets including both particle effects and HLSL shaders.</>,
     ];
