61ffcdaa3497a9951f4268d9be5deeef2b919b3f
mchrpt <mchrpt@yahoo.com>
2025-04-06 02:54:41 -0400

    Add in # of lines of code

--- src/Components/Projects/ProjectPages/Imperius/ContributionsSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/ContributionsSection.tsx
@@ -6,8 +6,8 @@ import BulletList from "../../BulletList";
 const ContributionsSection: React.FC = () => {
     const bullets = [
         <><span className="purple">Project Manager:</span> Recruited, coordinated, and managed a team of 6.</>,
-        <><span className="purple">Lead Programmer:</span> Wrote a majority of the project's codebase, as well as employed version control to manage other developers.</>,
-        <><span className="purple">Systems Programmer:</span> Designed and implemented several crucial game mechanics, including the economy, special abilities, and ship variants.</>,
+        <><span className="purple">Lead Programmer:</span>Authored most of the 15,000+ line codebase and managed version control for team collaboration.</>,
+        <><span className="purple">Systems Programmer:</span> Designed and implemented several crucial game mechanics listed below.</>,
         <><span className="purple">Audio Programmer:</span> Integrated FMOD middleware to ensure authentic and realistic soundscapes.</>,
         <><span className="purple">VFX Tech Artist:</span> Developed and adapted a comprehensive range of VFX assets including both particle effects and HLSL shaders.</>,
     ];
