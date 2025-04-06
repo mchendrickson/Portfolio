16d8c64aaaef8b064320de043926b6d80e801dd1
mchrpt <mchrpt@yahoo.com>
2025-04-06 13:53:48 -0400

    Update pathfinding section to be more clear

--- src/Components/Projects/ProjectPages/Imperius/PathfindingSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/PathfindingSection.tsx
@@ -10,13 +10,14 @@ const PathfindingSection: React.FC = () => {
         <><span className="purple">A* Pathfinding:</span> To generate the best possible path through the position nodes. </>,
         <><span className="purple">Catmull-Rom Splines:</span> To create a smooth, natural looking path for each unit to follow. </>,
         <><span className="purple">Tweening:</span> To create dynamic animations for each unit as they speed up, slow down, and rotate. </>,
+        <><span className="purple">Multithreading:</span> To offload A* and movement calculations to separate threads, increasing performance.</>,
     ];
 
     return (
         <Section title="3D Pathfinding">
             <p style={{textAlign: "left"}}>
-                I created a fast, efficient, and accurate <span className="purple">3D pathfinding</span> solution
-                that works with hundreds of agents simultaneously with little/no performance overhead.
+                I created a fast, efficient, and accurate <span className="purple">3D pathfinding</span> solution that
+                supports hundreds of agents with real-time performance and minimal CPU usage.
                 Units are able to move seamlessly in a 3D environment without colliding into any obstacles
                 and organize themselves efficiently into formations.
             </p>
