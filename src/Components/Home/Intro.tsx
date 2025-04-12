2be0e5b026c2546630e13236dd0d09cc9b99e0bc
mchrpt <mchrpt@yahoo.com>
2025-04-12 18:27:29 -0400

    Change name color

--- src/Components/Home/Intro.tsx
+++ src/Components/Home/Intro.tsx
@@ -31,7 +31,7 @@ const Intro: React.FC = () => {
                             </h1>
                         </AnimatedWrapper>
                         <h1 className="heading-name">
-                            I'M <span className="main-name">MATTHEW HENDRICKSON</span>
+                            I'M <span className="purple">MATTHEW HENDRICKSON</span>
                         </h1>
                         <Type/>
                     </Col>
