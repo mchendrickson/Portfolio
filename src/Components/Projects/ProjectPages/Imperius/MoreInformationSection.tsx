dfcd56a35907c639e5aa5d629850b48daa4bad3e
mchrpt <mchrpt@yahoo.com>
2025-10-11 21:19:20 -0400

    Add in WPI poster to more information section

--- src/Components/Projects/ProjectPages/Imperius/MoreInformationSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/MoreInformationSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Section from "../../Section";
+import ImperiusPresentation from "../../../../Assets/Projects/Imperius/ImperiusPresentation.png";
 
 /**
  * MoreInformationSection component for the Imperius project page.
@@ -12,6 +13,7 @@ const MoreInformationSection: React.FC = () => {
             <p style={{textAlign: "left"}}>
                 Please also see <span className="body-text"><a href="https://digital.wpi.edu/concern/student_works/zp38wh02w?locale=en" target="_blank" rel="noopener noreferrer">the full project report</a></span> as well as the <span className="body-text"><a href="https://github.com/mchendrickson/Portfolio/blob/master/src/Assets/Projects/Imperius/ImperiusREADME.md" target="_blank" rel="noopener noreferrer">credits, project members, and code citations</a></span>.
             </p>
+            <img src={ImperiusPresentation} alt="Imperius Presentation" className="img-fluid rounded" />
         </Section>
     );
 };
