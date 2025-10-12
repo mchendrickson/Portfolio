8e199b8794c5b794f3f5557811bf18e49b363c48
mchrpt <mchrpt@yahoo.com>
2025-10-12 14:40:43 -0400

    Fixes to More Information Section

--- src/Components/Projects/ProjectPages/Imperius/MoreInformationSection.tsx
+++ src/Components/Projects/ProjectPages/Imperius/MoreInformationSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section from "../../Section";
-import ImperiusPresentation from "../../../../Assets/Projects/Imperius/ImperiusPresentation.png";
+import ImperiusMQPReport from "../../../../Assets/Projects/Imperius/ImperiusMQPReport.png";
+import {LuMousePointerClick} from "react-icons/lu";
 
 /**
  * MoreInformationSection component for the Imperius project page.
@@ -11,9 +12,11 @@ const MoreInformationSection: React.FC = () => {
     return (
         <Section title="More Information">
             <p style={{textAlign: "left"}}>
-                Please also see <span className="body-text"><a href="https://digital.wpi.edu/concern/student_works/zp38wh02w?locale=en" target="_blank" rel="noopener noreferrer">the full project report</a></span> as well as the <span className="body-text"><a href="https://github.com/mchendrickson/Portfolio/blob/master/src/Assets/Projects/Imperius/ImperiusREADME.md" target="_blank" rel="noopener noreferrer">credits, project members, and code citations</a></span>.
+                Please also see <span className="body-text"><a href="https://digital.wpi.edu/concern/student_works/zp38wh02w?locale=en" target="_blank" rel="noopener noreferrer">the full project report <LuMousePointerClick size={25} className={"body-text"} /></a></span> as well as the <span className="body-text"><a href="https://github.com/mchendrickson/Portfolio/blob/master/src/Assets/Projects/Imperius/ImperiusREADME.md" target="_blank" rel="noopener noreferrer">credits, project members, and code citations <LuMousePointerClick size={25} className={"body-text"} /></a></span>.
             </p>
-            <img src={ImperiusPresentation} alt="Imperius Presentation" className="img-fluid rounded" />
+            <a href="https://digital.wpi.edu/concern/student_works/zp38wh02w?locale=en" target="_blank" rel="noopener noreferrer">
+                <img src={ImperiusMQPReport} alt="Imperius MQP Report" className="img-fluid rounded" style={{ width: '50%', height: 'auto' }} />
+            </a>
         </Section>
     );
 };
